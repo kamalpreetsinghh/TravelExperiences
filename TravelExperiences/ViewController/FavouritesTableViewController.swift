88edfa608323c825f82bdf0a69bd9aea07538059
Rahul Vashist <rhlvashist@gmail.com>
2021-12-03 03:00:18 -0500

    Delete favs

--- TravelExperiences/ViewController/FavouritesTableViewController.swift
+++ TravelExperiences/ViewController/FavouritesTableViewController.swift
@@ -11,7 +11,9 @@ class FavouritesTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.title = "Favourites"
         self.tableView.rowHeight = 150
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Delete All", style: .plain, target: self, action: #selector(deleteAll))
     }
 
     // MARK: - Table view data source
@@ -39,51 +41,25 @@ class FavouritesTableViewController: UITableViewController {
         cell.activityImg.image = UIImage(named: obj!.images[0])
         return cell
     }
+
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            var favList = UserDefaults.standard.array(forKey: "\(UsersDB.shared.currentUser.name).favorites") as? [String] ?? [String]()
+            favList.remove(at: indexPath.row)
+            UserDefaults.standard.set(favList, forKey: "\(UsersDB.shared.currentUser.name).favorites")
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    @IBAction func deleteAll() {
+        print("fsdfds")
+        var favList = UserDefaults.standard.array(forKey: "\(UsersDB.shared.currentUser.name).favorites") as? [String] ?? [String]()
+        favList.removeAll()
+        UserDefaults.standard.set(favList, forKey: "\(UsersDB.shared.currentUser.name).favorites")
+        [tableView .reloadData()]
+        
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
